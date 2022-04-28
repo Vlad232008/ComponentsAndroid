1728ea21c47a852370fb503b019ad06aeea9b174
Vlad232008 <vlad2320082009@gmail.com>
2022-04-28 08:25:59 +0400

    Service/BroadCast Receiver

--- app/src/main/java/com/example/components/component/MessageReceiver.kt
+++ app/src/main/java/com/example/components/component/MessageReceiver.kt
@@ -3,11 +3,16 @@ package com.example.components.component
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class MessageReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        TODO("MessageReceiver.onReceive() is not implemented")
+        val isAirplaneModeEnabled = intent.getBooleanExtra("state",false)
+        if (isAirplaneModeEnabled){
+            Toast.makeText(context, "Режим полета включен", Toast.LENGTH_LONG).show()
+        }
+        else Toast.makeText(context, "Режим полета выключен", Toast.LENGTH_LONG).show()
     }
 }
